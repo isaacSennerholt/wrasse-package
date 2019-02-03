b77e7cf8bba8eb080dcfe20b9ec8c4d2fc57fa72
Isaac Sennerholt <isaac.sennerholt@gmail.com>
2019-02-03 17:08:19 +0100

    Add duck extension variable

--- createPackageDuck.js
+++ createPackageDuck.js
@@ -3,20 +3,22 @@ import { extendDuck } from "wrasse-duck-extend";
 
 export default function(duck, { mountReducer, extensions } = {}) {
   if (!isPlainObject(duck)) throw new Error("Duck has to be a plain object");
+  
+  let extendedDuck = duck
 
   if (Array.isArray(extensions)) {
     extensions.forEach(extension => {
       const extensionObject =
-        typeof extension === "function" ? extension(duck) : extension;
+        typeof extension === "function" ? extension(extendedDuck) : extension;
 
-      duck = extendDuck(duck, extensionObject);
+        extendedDuck = extendDuck(extendedDuck, extensionObject);
     });
   }
 
   if (typeof mountReducer === "function") {
-    const { name, reducer } = duck;
+    const { name, reducer } = extendedDuck;
     mountReducer(name, reducer);
   }
 
-  return { ...duck };
+  return { ...extendedDuck };
 }
